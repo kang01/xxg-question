25ffb7875714a81828ad9b2ae80720524a86354e
GengLuying <luying.geng@fwoxford.org>
2018-06-20 14:03:54 +0800

    发送问题

--- src/main/java/org/fwoxford/service/impl/MailServiceImpl.java
+++ src/main/java/org/fwoxford/service/impl/MailServiceImpl.java
@@ -4,12 +4,15 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import org.fwoxford.service.MailService;
 import org.fwoxford.service.dto.MessagerDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import javax.mail.MessagingException;
@@ -26,6 +29,7 @@ import java.util.Map;
 @Service
 @Transactional
 public class MailServiceImpl implements MailService {
+    private final Logger log = LoggerFactory.getLogger(MailService.class);
     //注入MailSender
     @Autowired
     JavaMailSender mailSender;
@@ -44,11 +48,15 @@ public class MailServiceImpl implements MailService {
     @Override
     public void sendMessageMail(Object params, String title, String templateName, MessagerDTO messagerDTO) {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
+        Boolean flag = true;
         MimeMessageHelper helper = null;
         try {
             helper = new MimeMessageHelper(mimeMessage, true);
             helper.setFrom(messagerDTO.getFromUser());
             helper.setTo(InternetAddress.parse(messagerDTO.getToUser()));//发送给谁
+            if(StringUtils.isEmpty(title)){
+                title = "问题样本确认";
+            }
             helper.setSubject(title);
             Map<String, Object> model = new HashMap<>();
             model.put("params", params);
@@ -60,6 +68,7 @@ public class MailServiceImpl implements MailService {
 
                     helper.setText(text, true);
                     mailSender.send(mimeMessage);
+                    log.debug("Sent e-mail to User '{}'", messagerDTO.getToUser());
                 } catch (TemplateException e) {
                     e.printStackTrace();
                 }
@@ -68,6 +77,8 @@ public class MailServiceImpl implements MailService {
             }
         } catch (MessagingException e) {
             e.printStackTrace();
+            log.warn("E-mail could not be sent to user '{}'", messagerDTO.getToUser(), e);
+            flag = false;
         }
 
 
